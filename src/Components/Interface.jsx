a8353f8d8dca08512bd7c4c3b810dd2273876657
shubham09anand <shubhamsheelam@gmail.com>
2024-07-21 16:48:58 +0530

    init

--- src/Components/Interface.jsx
+++ src/Components/Interface.jsx
@@ -212,7 +212,7 @@ const Interface = () => {
                          </div>
                     </div>
 
-                    <div className="flex bg-white flex-wrap content-center justify-center rounded-r-md mx-auto w-40 h-40 shadow-2xl rounded-4xl">
+                    <div className="flex bg-white flex-wrap content-center justify-center rounded-r-md mx-auto shadow-2xl rounded-4xl">
                          {qrCodeUrl ? (
                               <canvas
                                    ref={canvasRef}
