fa018a2829b45987143fb669d53c97584d7d168d
shubham09anand <shubhamsheelam@gmail.com>
2024-08-20 23:32:43 +0530

    init

--- src/Components/Interface.jsx
+++ src/Components/Interface.jsx
@@ -73,8 +73,9 @@ const Interface = () => {
 
      useEffect(() => {
           handleGenerateQRCode();
+          // eslint-disable-next-line
      }, [dimension, dotOption, color, backgroundColor, data, image, padding]);
-
+// eslint-disable-next-line
      const handleDownload = () => {
           if (dimension < 50 || dimension > 2500) {
                setDimension(600);
@@ -92,26 +93,60 @@ const Interface = () => {
      };
 
      return (
-          <div className="min-h-screen w-screen content-center justify-center lg:p-5">
-               <div className="-translate-y-20 md:translate-y-0 flex place-content-center items-center flex-col-reverse p-1 lg:space-x lg:flex-row shadow-md w-full lg:w-3/4 mx-auto rounded-3xl">
-                    <div className="-translate-y-20 md:translate-y-0 flex place-content-center items-center justify-center rounded-l-md mt-5 lg:w-1/2">
-                         <div className="w-full sm:shadowBox border-2 p-5 rounded-md">
-                              <Link to='/NormalQRCode' className='text-blue-500'>Genrate Normal QR Code</Link>
-                              <div className="text-black font-semibold text-3xl">QR Code Generator</div>
-                              <form className="mt-4">
-                                   <div className="mb-3">
-                                        <label className="mb-2 block">Dimension</label>
+          <>
+
+               <div style={{ backgroundImage: "url('https://img.freepik.com/free-vector/wave-gradient-blue-background-modern-design_343694-3814.jpg')", backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', width: '100%', height: '100%' }} className="h-full w-screen content-center justify-center lg:p-5">
+                    <div className="lg:h-screen lg:gap-x-10 flex place-content-center items-center flex-col-reverse lg:flex-row w-full lg:w-11/12 mx-auto">
+                         <div>
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="white" className="bi bi-qr-code-scan size-40 absolute right-10 top-40 opacity-30" viewBox="0 0 16 16">
+                                   <path d="M0 .5A.5.5 0 0 1 .5 0h3a.5.5 0 0 1 0 1H1v2.5a.5.5 0 0 1-1 0zm12 0a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0V1h-2.5a.5.5 0 0 1-.5-.5M.5 12a.5.5 0 0 1 .5.5V15h2.5a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H15v-2.5a.5.5 0 0 1 .5-.5M4 4h1v1H4z" />
+                                   <path d="M7 2H2v5h5zM3 3h3v3H3zm2 8H4v1h1z" />
+                                   <path d="M7 9H2v5h5zm-4 1h3v3H3zm8-6h1v1h-1z" />
+                                   <path d="M9 2h5v5H9zm1 1v3h3V3zM8 8v2h1v1H8v1h2v-2h1v2h1v-1h2v-1h-3V8zm2 2H9V9h1zm4 2h-1v1h-2v1h3zm-4 2v-1H8v1z" />
+                                   <path d="M12 9h2V8h-2z" />
+                              </svg>
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="white" className="bi bi-qr-code-scan size-16 absolute left-10 top-20 opacity-20" viewBox="0 0 16 16">
+                                   <path d="M0 .5A.5.5 0 0 1 .5 0h3a.5.5 0 0 1 0 1H1v2.5a.5.5 0 0 1-1 0zm12 0a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0V1h-2.5a.5.5 0 0 1-.5-.5M.5 12a.5.5 0 0 1 .5.5V15h2.5a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H15v-2.5a.5.5 0 0 1 .5-.5M4 4h1v1H4z" />
+                                   <path d="M7 2H2v5h5zM3 3h3v3H3zm2 8H4v1h1z" />
+                                   <path d="M7 9H2v5h5zm-4 1h3v3H3zm8-6h1v1h-1z" />
+                                   <path d="M9 2h5v5H9zm1 1v3h3V3zM8 8v2h1v1H8v1h2v-2h1v2h1v-1h2v-1h-3V8zm2 2H9V9h1zm4 2h-1v1h-2v1h3zm-4 2v-1H8v1z" />
+                                   <path d="M12 9h2V8h-2z" />
+                              </svg>
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="white" className="bi bi-qr-code-scan size-40 absolute bottom-10 opacity-30" viewBox="0 0 16 16">
+                                   <path d="M0 .5A.5.5 0 0 1 .5 0h3a.5.5 0 0 1 0 1H1v2.5a.5.5 0 0 1-1 0zm12 0a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0V1h-2.5a.5.5 0 0 1-.5-.5M.5 12a.5.5 0 0 1 .5.5V15h2.5a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H15v-2.5a.5.5 0 0 1 .5-.5M4 4h1v1H4z" />
+                                   <path d="M7 2H2v5h5zM3 3h3v3H3zm2 8H4v1h1z" />
+                                   <path d="M7 9H2v5h5zm-4 1h3v3H3zm8-6h1v1h-1z" />
+                                   <path d="M9 2h5v5H9zm1 1v3h3V3zM8 8v2h1v1H8v1h2v-2h1v2h1v-1h2v-1h-3V8zm2 2H9V9h1zm4 2h-1v1h-2v1h3zm-4 2v-1H8v1z" />
+                                   <path d="M12 9h2V8h-2z" />
+                              </svg>
+                         </div>
+
+                         <div className="-translate-y-20 lg:translate-y-0 flex place-content-center items-center justify-center rounded-l-md mt-5 w-full sm:w-3/4">
+                              <div className="w-full sm:shadowBox p-5 rounded-md">
+                                   <Link to='/NormalQRCode' className='text-white mb-2 pl-2'>Genrate Normal QR Code</Link>
+                                   <div>
+                                        <label className="mb-5 block font-semibold text-4xl lg:text-5xl text-blue-950">Enter your text</label>
+                                        <input
+                                             type="text"
+                                             placeholder="Start Typing...."
+                                             value={data}
+                                             onChange={(e) => setData(e.target.value)}
+                                             className="block w-full bg-transparent border-b text-lg border-gray-300 focus:outline-none py-1 px-1.5 text-white"
+                                        />
+                                   </div>
+                                   <div>
+                                        <label className="mb-2 block text-xl font-semibold">Dimension</label>
                                         <input
                                              type="number"
                                              placeholder="Enter the dimension (size x size)"
                                              value={dimension}
                                              onChange={(e) => setDimension(e.target.value)}
-                                             className="block w-full rounded-md border border-gray-300 focus:outline-none py-1 px-1.5 text-gray-500"
+                                             className="block w-full border-b text-lg border-gray-300 focus:outline-none py-1 px-1.5 text-white bg-transparent"
                                         />
                                    </div>
 
-                                   <div className="mb-2 block">Dot Options</div>
-                                   <div className="mb-3 flex flex-wrap">
+                                   <div className="mb-5 flex flex-wrap">
+                                        <div className="mb-2 block text-xl font-semibold">Dot Options</div>
                                         <div className="flex items-center m-2">
                                              <input
                                                   id="dotOption-rounded"
@@ -119,9 +154,9 @@ const Interface = () => {
                                                   value="rounded"
                                                   checked={dotOption === 'rounded'}
                                                   onChange={(e) => setDotOption(e.target.value)}
-                                                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"
+                                                  className="w-4 h-4 border-black border-2 bg-gray-100 outline-none"
                                              />
-                                             <label htmlFor="dotOption-rounded" className="ms-2 text-sm font-medium text-gray-900">Rounded</label>
+                                             <label htmlFor="dotOption-rounded" className="ms-2 text-sm font-bold text-gray-900">Rounded</label>
                                         </div>
                                         <div className="flex items-center m-2">
                                              <input
@@ -130,9 +165,9 @@ const Interface = () => {
                                                   value="dots"
                                                   checked={dotOption === 'dots'}
                                                   onChange={(e) => setDotOption(e.target.value)}
-                                                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"
+                                                  className="w-4 h-4 border-black border-2 bg-gray-100 outline-none"
                                              />
-                                             <label htmlFor="dotOption-dots" className="ms-2 text-sm font-medium text-gray-900">Dots</label>
+                                             <label htmlFor="dotOption-dots" className="ms-2 text-sm font-bold text-gray-900">Dots</label>
                                         </div>
                                         <div className="flex items-center m-2">
                                              <input
@@ -141,9 +176,9 @@ const Interface = () => {
                                                   value="classy"
                                                   checked={dotOption === 'classy'}
                                                   onChange={(e) => setDotOption(e.target.value)}
-                                                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"
+                                                  className="w-4 h-4 border-black border-2 bg-gray-100 outline-none"
                                              />
-                                             <label htmlFor="dotOption-classy" className="ms-2 text-sm font-medium text-gray-900">Classy Rounded</label>
+                                             <label htmlFor="dotOption-classy" className="ms-2 text-sm font-bold text-gray-900">Classy Rounded</label>
                                         </div>
                                         <div className="flex items-center m-2">
                                              <input
@@ -152,9 +187,9 @@ const Interface = () => {
                                                   value="classy-rounded"
                                                   checked={dotOption === 'classy-rounded'}
                                                   onChange={(e) => setDotOption(e.target.value)}
-                                                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"
+                                                  className="w-4 h-4 border-black border-2 bg-gray-100 outline-none"
                                              />
-                                             <label htmlFor="dotOption-classy-rounded" className="ms-2 text-sm font-medium text-gray-900">Classy Rounded</label>
+                                             <label htmlFor="dotOption-classy-rounded" className="ms-2 text-sm font-bold text-gray-900">Classy Rounded</label>
                                         </div>
                                         <div className="flex items-center m-2">
                                              <input
@@ -163,9 +198,9 @@ const Interface = () => {
                                                   value="square"
                                                   checked={dotOption === 'square'}
                                                   onChange={(e) => setDotOption(e.target.value)}
-                                                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"
+                                                  className="w-4 h-4 border-black border-2 bg-gray-100 outline-none"
                                              />
-                                             <label htmlFor="dotOption-square" className="ms-2 text-sm font-medium text-gray-900">Square</label>
+                                             <label htmlFor="dotOption-square" className="ms-2 text-sm font-bold text-gray-900">Square</label>
                                         </div>
                                         <div className="flex items-center m-2">
                                              <input
@@ -174,67 +209,58 @@ const Interface = () => {
                                                   value="extra-rounded"
                                                   checked={dotOption === 'extra-rounded'}
                                                   onChange={(e) => setDotOption(e.target.value)}
-                                                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"
+                                                  className="w-4 h-4 border-black border-2 bg-gray-100 outline-none"
                                              />
-                                             <label htmlFor="dotOption-extra-rounded" className="ms-2 text-sm font-medium text-gray-900">Extra Rounded</label>
+                                             <label htmlFor="dotOption-extra-rounded" className="ms-2 text-sm font-bold text-gray-900">Extra Rounded</label>
                                         </div>
                                    </div>
 
-                                   <div className="mb-3">
-                                        <label className="mb-2 block">Color of QR Code</label>
+                                   <div>
+                                        <label className="mb-2 block text-xl font-semibold">Color of QR Code</label>
                                         <input
                                              type="color"
                                              value={color}
                                              onChange={(e) => setColor(e.target.value)}
-                                             className="block w-full rounded-md border border-gray-300 focus:outline-none py-1 px-1.5 text-gray-500"
+                                             className="block w-full rounded-md focus:outline-none py-1 px-1.5 text-gray-500"
                                         />
                                    </div>
 
-                                   <div className="mb-3">
-                                        <label className="mb-2 block">Background Color</label>
+                                   <div>
+                                        <label className="mb-2 block text-xl font-semibold">Background Color</label>
                                         <input
                                              type="color"
                                              value={backgroundColor}
                                              onChange={(e) => setBackgroundColor(e.target.value)}
-                                             className="block w-full rounded-md border border-gray-300 focus:outline-none py-1 px-1.5 text-gray-500"
+                                             className="block w-full focus:outline-none py-1 px-1.5 text-gray-500"
                                         />
                                    </div>
+                              </div>
+                         </div>
 
-                                   <div className="mb-3">
-                                        <label className="mb-2 block">Link/Data/Info</label>
-                                        <input
-                                             type="text"
-                                             placeholder="Enter the data"
-                                             value={data}
-                                             onChange={(e) => setData(e.target.value)}
-                                             className="block w-full rounded-md border border-gray-300 focus:outline-none py-1 px-1.5 text-gray-500"
-                                        />
+
+                         <div className="flex flex-col place-content-center items-center scale-[60%]">
+
+                              {qrCodeUrl ? (
+                                   <canvas
+                                        ref={canvasRef}
+                                        className={`w-[${parseInt(dimension, 10) + parseInt(padding, 10) * 2}px] h-[${parseInt(dimension, 10) + parseInt(padding, 10) * 2}px] bg-center rounded-3xl bg-no-repeat bg-cover  lg:mt-0`}
+                                   />
+                              ) : (
+                                   <div className="w-full h-full rounded-r-md flex items-center justify-center">
+                                        <span className="w-full text-center">QR Code Preview</span>
                                    </div>
+                              )}
+                              <button type="button" onClick={handleDownload} className="hover:bg-blue-400 flex place-content-center items-center gap-x-5 mt-10 w-fit h-fit font-semibold px-5 py-4 bg-white text-black text-2xl rounded-full">
+                                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="size-9">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                                   </svg>
 
-                                   <button type="button" onClick={handleDownload} className="block w-full bg-purple-700 text-white rounded-md py-2 mt-4">
-                                        Generate & Download QR Code
-                                   </button>
-                              </form>
+                                   Generate & Download QR Code
+                              </button>
                          </div>
                     </div>
-
-                    <div className="flex -translate-x-20 md:translate-x-10 scale-50 md:scale-75 bg-white flex-wrap content-center justify-center rounded-r-md mx-auto shadow-2xl rounded-4xl">
-                         <div className="w-full h-full rounded-r-md flex items-center justify-center">
-                                   <span className="w-full text-center">QR Code appear here,start creating Your QR Ocde</span>
-                              </div>
-                         {qrCodeUrl ? (
-                              <canvas
-                                   ref={canvasRef}
-                                   className={`w-[${parseInt(dimension, 10) + parseInt(padding, 10) * 2}px] h-[${parseInt(dimension, 10) + parseInt(padding, 10) * 2}px] bg-center rounded-3xl bg-no-repeat bg-cover`}
-                              />
-                         ) : (
-                              <div className="w-full h-full rounded-r-md flex items-center justify-center">
-                                   <span className="w-full text-center">QR Code Preview</span>
-                              </div>
-                         )}
-                    </div>
                </div>
-          </div>
+          </>
      );
 };
 
--- src/Components/QrCode.jsx
+++ src/Components/QrCode.jsx
@@ -2,6 +2,7 @@ import React, { useState, useRef } from 'react';
 import QRCode from 'qrcode.react';
 import "../App.css";
 import { Link } from 'react-router-dom';
+import back from '../Assets/back.jpg'
 
 const QrCode = () => {
   const [data, setData] = useState('');
@@ -35,31 +36,73 @@ const QrCode = () => {
   };
 
   return (
-    <div className="text-wxl font-semibold mt-5 space-y-3 w-full md:w-1/2 mx-auto flex flex-col place-content-center items-center">
-      <h1 className='text-2xl font-semibold'>QR Code Generator</h1>
-      <input
-        type="text"
-        value={data}
-        onChange={(e) => setData(e.target.value)}
-        placeholder="Enter data"
-        className="text-gray-600 border-2 w-4/5 md:w-1/2 p-1 border-gray-500 rounded-sm outline-none focus:bg-gray-100"
-      />
-      {data ? (
+    <div style={{ backgroundImage: `url(${back})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', width: '100%', height: '100%' }} className="text-wxl font-semibold space-y-3 w-full md:w-1/2 mx-auto flex flex-col place-content-center items-center">
+      <div className='h-screen w-full flex flex-col items-center'>
         <div>
-          <div className='text-xl font-semibold my-2 text-center'>Generated QR Code:</div>
-          <div ref={qrRef}>
-            <QRCode value={data} size={256} />
-          </div>
-          <div onClick={downloadQRCode} className="active:opacity-70 cursor-pointer text-white text-center bg-blue-600 mt-5 p-2 rounded-sm">
-            Download QR Code as PNG
-          </div>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="black" className="bi bi-qr-code-scan size-40 absolute right-10 top-40 opacity-50 scale-[10%] md:scale-50 lg:scale-90" viewBox="0 0 16 16">
+            <path d="M0 .5A.5.5 0 0 1 .5 0h3a.5.5 0 0 1 0 1H1v2.5a.5.5 0 0 1-1 0zm12 0a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0V1h-2.5a.5.5 0 0 1-.5-.5M.5 12a.5.5 0 0 1 .5.5V15h2.5a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H15v-2.5a.5.5 0 0 1 .5-.5M4 4h1v1H4z" />
+            <path d="M7 2H2v5h5zM3 3h3v3H3zm2 8H4v1h1z" />
+            <path d="M7 9H2v5h5zm-4 1h3v3H3zm8-6h1v1h-1z" />
+            <path d="M9 2h5v5H9zm1 1v3h3V3zM8 8v2h1v1H8v1h2v-2h1v2h1v-1h2v-1h-3V8zm2 2H9V9h1zm4 2h-1v1h-2v1h3zm-4 2v-1H8v1z" />
+            <path d="M12 9h2V8h-2z" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="#52b1dd" className="bi bi-qr-code-scan size-40 absolute left-10 top-80 opacity-80 scale-[20%] md:scale-50 lg:scale-90" viewBox="0 0 16 16">
+            <path d="M0 .5A.5.5 0 0 1 .5 0h3a.5.5 0 0 1 0 1H1v2.5a.5.5 0 0 1-1 0zm12 0a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0V1h-2.5a.5.5 0 0 1-.5-.5M.5 12a.5.5 0 0 1 .5.5V15h2.5a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H15v-2.5a.5.5 0 0 1 .5-.5M4 4h1v1H4z" />
+            <path d="M7 2H2v5h5zM3 3h3v3H3zm2 8H4v1h1z" />
+            <path d="M7 9H2v5h5zm-4 1h3v3H3zm8-6h1v1h-1z" />
+            <path d="M9 2h5v5H9zm1 1v3h3V3zM8 8v2h1v1H8v1h2v-2h1v2h1v-1h2v-1h-3V8zm2 2H9V9h1zm4 2h-1v1h-2v1h3zm-4 2v-1H8v1z" />
+            <path d="M12 9h2V8h-2z" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="black" className="bi bi-qr-code-scan size-16 absolute left-5 top-28 opacity-70 scale-[50%] md:scale-50 lg:scale-90" viewBox="0 0 16 16">
+            <path d="M0 .5A.5.5 0 0 1 .5 0h3a.5.5 0 0 1 0 1H1v2.5a.5.5 0 0 1-1 0zm12 0a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0V1h-2.5a.5.5 0 0 1-.5-.5M.5 12a.5.5 0 0 1 .5.5V15h2.5a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H15v-2.5a.5.5 0 0 1 .5-.5M4 4h1v1H4z" />
+            <path d="M7 2H2v5h5zM3 3h3v3H3zm2 8H4v1h1z" />
+            <path d="M7 9H2v5h5zm-4 1h3v3H3zm8-6h1v1h-1z" />
+            <path d="M9 2h5v5H9zm1 1v3h3V3zM8 8v2h1v1H8v1h2v-2h1v2h1v-1h2v-1h-3V8zm2 2H9V9h1zm4 2h-1v1h-2v1h3zm-4 2v-1H8v1z" />
+            <path d="M12 9h2V8h-2z" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="black" className="bi bi-qr-code-scan size-40 absolute bottom-10 opacity-50 scale-[80%]" viewBox="0 0 16 16">
+            <path d="M0 .5A.5.5 0 0 1 .5 0h3a.5.5 0 0 1 0 1H1v2.5a.5.5 0 0 1-1 0zm12 0a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0V1h-2.5a.5.5 0 0 1-.5-.5M.5 12a.5.5 0 0 1 .5.5V15h2.5a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5v-3a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H15v-2.5a.5.5 0 0 1 .5-.5M4 4h1v1H4z" />
+            <path d="M7 2H2v5h5zM3 3h3v3H3zm2 8H4v1h1z" />
+            <path d="M7 9H2v5h5zm-4 1h3v3H3zm8-6h1v1h-1z" />
+            <path d="M9 2h5v5H9zm1 1v3h3V3zM8 8v2h1v1H8v1h2v-2h1v2h1v-1h2v-1h-3V8zm2 2H9V9h1zm4 2h-1v1h-2v1h3zm-4 2v-1H8v1z" />
+            <path d="M12 9h2V8h-2z" />
+          </svg>
+
         </div>
-      ) :
-        <div className='font-extralight h-40 w-4/5 md:w-1/2 text-center pt-5 bg-gray-200'>
-          Your QR Genrate Here. Enter the text in text Box first.  <br /> OR <br />
-          <Link to="/" className='text-blue-400'> click here to gerate more intractive QR code</Link>
+        <div className='text-4xl font-bold pt-12 font-serif'>QR Code Generator</div>
+        <input
+          type="text"
+          value={data}
+          onChange={(e) => setData(e.target.value)}
+          placeholder="This text will become QR code"
+          className="w-4/5 md:w-1/3 p-1 mt-10 border-gray-800 focus:border-gray-900 border-2 rounded-sm outline-none focus:bg-gray-100 placeholder:text-gray-500"
+        />
+        {data ? (
+          <div>
+            <div className='text-xl font-semibold my-2 text-center'>Generated QR Code:</div>
+            <div ref={qrRef}>
+              <QRCode value={data} size={256} />
+            </div>
+            <div onClick={downloadQRCode} className="active:opacity-70 cursor-pointer text-white text-center bg-blue-600 mt-5 p-2 rounded-sm">
+              Download QR Code as PNG
+            </div>
+          </div>
+        ) :
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-qr-code size-40 mt-10" viewBox="0 0 16 16">
+            <path d="M2 2h2v2H2z" />
+            <path d="M6 0v6H0V0zM5 1H1v4h4zM4 12H2v2h2z" />
+            <path d="M6 10v6H0v-6zm-5 1v4h4v-4zm11-9h2v2h-2z" />
+            <path d="M10 0v6h6V0zm5 1v4h-4V1zM8 1V0h1v2H8v2H7V1zm0 5V4h1v2zM6 8V7h1V6h1v2h1V7h5v1h-4v1H7V8zm0 0v1H2V8H1v1H0V7h3v1zm10 1h-1V7h1zm-1 0h-1v2h2v-1h-1zm-4 0h2v1h-1v1h-1zm2 3v-1h-1v1h-1v1H9v1h3v-2zm0 0h3v1h-2v1h-1zm-4-1v1h1v-2H7v1z" />
+            <path d="M7 12h1v3h4v1H7zm9 2v2h-3v-1h2v-1z" />
+          </svg>
+        }
+
+        <div className='font-extralight h-40 w-4/5 md:w-1/2 text-center pt-5 '>
+          {data === "" && (<div>Your QR Genrate Here. Enter the text in text Box first.  <br /> OR <br /></div>)}
+
+          <Link to="/" className='text-blue-800'> click here to gerate more intractive QR code</Link>
         </div>
-      }
+      </div>
     </div>
   )
 }
